1c76382163673306760cb583734d9f0922cdda84
dusanlekic22 <lekic.ra159.2018@uns.ac.rs>
2022-09-04 02:13:18 +0200

    feat: added recommend for one user

--- src/main.py
+++ src/main.py
@@ -74,6 +74,31 @@ def evaluate(ratings, test_ratings):
     print("The Hit Ratio @ 10 is {:.2f}".format(np.average(hits)))
 
 
+def get_recommendations(ratings, test_ratings, user_id):
+    movies = pd.read_csv('../data/movies.csv')
+    test_user_item_set = set(zip(test_ratings['userId'], test_ratings['movieId']))
+
+    # Dict of all items that are interacted with by each user
+    user_interacted_items = ratings.groupby('userId')['movieId'].apply(list).to_dict()
+
+    for (u, i) in test_user_item_set:
+        if u == user_id:
+            interacted_items = user_interacted_items[user_id]
+            not_interacted_items = set(all_movieIds) - set(interacted_items)
+            selected_not_interacted = list(np.random.choice(list(not_interacted_items), 99))
+            test_items = selected_not_interacted + [i]
+            predicted_labels = np.squeeze(model(torch.tensor([u] * 100),
+                                                torch.tensor(test_items)).detach().numpy())
+            top10_items = [test_items[i] for i in np.argsort(predicted_labels)[::-1][0:10].tolist()]
+            print(top10_items)
+            recommendations = ''
+            for it in top10_items:
+                recommendations += movies.loc[movies['movieId'] == it, 'title'].item() + '\n'
+            print("Tested is:", movies.loc[movies['movieId'] == i, 'title'].item())
+            print("We recommended you:\n", recommendations)
+
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
@@ -86,9 +111,7 @@ if __name__ == '__main__':
     num_users = data['userId'].max() + 1
     num_items = data['movieId'].max() + 1
     all_movieIds = data['movieId'].unique()
-    print(train_data.dtypes)
     checkpoint = torch.load('checkpoints/epoch=0-step=2520-v1.ckpt', map_location=lambda storage, loc: storage)
-    print(checkpoint["hyper_parameters"])
     hyper_params = checkpoint["hyper_parameters"]
 
     model = NCF.load_from_checkpoint('checkpoints/epoch=0-step=2520-v1.ckpt',
@@ -102,6 +125,12 @@ if __name__ == '__main__':
         trainer = pl.Trainer(max_epochs=1, reload_dataloaders_every_epoch=True,
                          progress_bar_refresh_rate=50, logger=False)
         trainer.fit(model)
-
-    evaluate(data, test_data)
+    while True:
+        print("Overall testing(1) or single user testing(2)?")
+        if input() == '1':
+            evaluate(data, test_data)
+        else:
+            print("User Id:")
+            x = input()
+            get_recommendations(data, test_data, x)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
